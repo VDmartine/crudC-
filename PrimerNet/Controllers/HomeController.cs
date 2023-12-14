2e869c34a95dba1d67d3807dfc36ed9260826218
victor D Anda <victormartinezdeand2@gmail.com>
2023-12-14 09:50:23 -0600

    first commit

---
+++ PrimerNet/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerNet.Models;
+using System.Diagnostics;
+using System.Data.SqlClient;
+namespace PrimerNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string cadenaSQL;
+
+        public HomeController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult ListaProductos()
+        {
+            List<Productos> lista = new List<Productos>();
+            using (var conexion = new SqlConnection(cadenaSQL))
+            {
+                conexion.Open();
+                var cmd = new SqlCommand("SP_Producto", conexion);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Productos
+                        {
+                            idProducto = Convert.ToInt32(dr["idProducto"]),
+                            Nombre = dr["nombre"].ToString(),
+                            Categoria = Convert.ToInt32(dr["categoria"]),
+                            SubCategorias = Convert.ToInt32(dr["subcategoria"]),
+                            idVendedor = Convert.ToInt32(dr["idVendedor"]),
+                            Calificacion = float.Parse(dr["calificacion"].ToString()),
+                            Unidades = Convert.ToInt32(dr["unidades"]),
+                            Imagen = dr["imagen"].ToString(),
+                            Descripcion = dr["descripcion"].ToString(),
+                        });
+                    }
+                }
+            }
+            return Json(new { data = lista });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
---
+++ PrimerNet/Models/Productos.cs
@@ -0,0 +1,16 @@
+﻿namespace PrimerNet.Models
+{
+    public class Productos
+    {
+        public int idProducto { get; set; }
+        public string Nombre { get; set; }
+        public double Cosoto { get; set; }
+        public int Categoria { get; set; }
+        public int SubCategorias { get; set;}
+        public int idVendedor { get; set;}
+        public float Calificacion { get; set; }
+        public int Unidades { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set;}
+    }
+}
